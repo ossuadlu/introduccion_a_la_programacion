c6d4e415bd0f66b2a7a24623d30c778b91653596
adny osorio <huertaslina61@gmail.com>
2021-11-22 19:34:34 -0500

    nueva foto

--- bootstrap_form/from_bootsrap.js
+++ bootstrap_form/from_bootsrap.js
@@ -1,20 +1,15 @@
-// Capturar un elemento de tipo form dentro del DOM
-const form = document.querySelector("form")
 
-// Mostrar el contenido del elemento form
+const form = document.querySelector("form")
 console.log(form)
 
-// Validar que form no sea un null
 if(form){
     console.log('it is a form');
 
-    // Crear un Listener para que JS reaccione a un evento
-    // En este caso el evento es submit.
-    // Cuando JS capture el evento, ejecutará la función
+
     form.addEventListener('submit', function(event){
         console.log('Sending');
 
-        // Prevenir la acción por defecto del evento
+    
         event.preventDefault()
 
         const email = document.querySelector('#email')
