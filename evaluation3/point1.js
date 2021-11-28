81e1dc191a020d6563fc98753bc0c4445026e788
adny osorio <huertaslina61@gmail.com>
2021-11-28 10:11:58 -0500

    intro_globant

--- evaluation3/point1.js
+++
@@ -1,8 +0,0 @@
-
-let names = prompt('Ingrese el nombre ruta')
-let subjects = prompt('Ingrese cantidad pasajeros transportados')
-let price = prompt('Ingrese valor del pasaje')
-
-subtotal = subjects * price
-subtotal = Number(subjects) * Number(price)
-alert('Hi ' + names + '. Please to pay ' + total)
